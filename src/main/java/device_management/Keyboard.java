5c49b51c2352769574b423aebf1fa8048d94f905
Joe McColl <joe4kool@gmail.com>
2019-02-20 14:35:04 +0000

    Add Keyboard class methods and tests

--- src/main/java/device_management/Keyboard.java
+++ src/main/java/device_management/Keyboard.java
@@ -4,10 +4,25 @@ import behaviours.IINput;
 
 public class Keyboard implements IINput {
 
+	String type;
+	int numberOfButtons;
+
+	public Keyboard(String type, int numberOfButtons){
+		this.type = type;
+		this.numberOfButtons = numberOfButtons;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getNumberOfButtons() {
+		return numberOfButtons;
+	}
 
 	@Override
 	public String sendData(String data) {
-		return null;
+		return "Buttons pressed" + data;
 	}
 
 }
--- src/test/java/KeyboardTest.java
+++ src/test/java/KeyboardTest.java
@@ -1,2 +1,31 @@
+import device_management.Keyboard;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 public class KeyboardTest {
+
+	Keyboard keyboard;
+
+	@Before
+	public void before(){
+		keyboard = new Keyboard("QWERTY", 100);
+	}
+
+	@Test
+	public void canGetType(){
+		assertEquals("QWERTY", keyboard.getType());
+	}
+
+	@Test
+	public void canGetNumberOfButtons(){
+		assertEquals(100, keyboard.getNumberOfButtons());
+	}
+
+	@Test
+	public void canSendData(){
+		assertEquals("Buttons pressed ABC", keyboard.sendData(" ABC"));
+	}
 }
