afa0f6fe2010d359c1dabe14ec188a0cd0f3bef3
Joe McColl <joe4kool@gmail.com>
2019-02-20 14:15:49 +0000

    added IINput and implemented within keyboard and mouse classes

---
+++ src/main/java/device_management/Keyboard.java
@@ -0,0 +1,13 @@
+package device_management;
+
+import behaviours.IINput;
+
+public class Keyboard implements IINput {
+
+
+	@Override
+	public String sendData() {
+		return null;
+	}
+	
+}
---
+++ src/main/java/device_management/Mouse.java
@@ -0,0 +1,12 @@
+package device_management;
+
+import behaviours.IINput;
+
+public class Mouse implements IINput {
+
+	@Override
+	public String sendData() {
+		return null;
+	}
+
+}
